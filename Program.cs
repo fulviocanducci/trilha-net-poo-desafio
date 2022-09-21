868796fe4bd4d3cf6e7e40815012cd90ca0e7d80
Fúlvio <fulviocanducci@hotmail.com>
2022-09-21 00:30:00 -0300

    Smartphone

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia");
+Smartphone nokia = new Nokia("1", "Nokia 100", "10112255360", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Telegram");
+
+Console.WriteLine("------------------------------------------------------------");
+
+Console.WriteLine("IPhone");
+Smartphone iphone = new Iphone("2", "Iphone 10", "580112255369", 512);
+iphone.Ligar();
+iphone.InstalarAplicativo("Photos");
